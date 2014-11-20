5d1f69c121d036898b9d1c9b13c09b71b31eb57d
lwasser <lwasser@neoninc.org>
2014-11-19 20:19:40 -0700

    Creating git python directory

---
+++ testHDF5code.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 19 18:40:32 2014
+
+@author: law
+"""
+
+import h5py
+import numpy
+f = h5py.File('new.h5', 'a')
+#dset= f['new']
+
+#f['dset'] = numpy.zeros((6,4), dtype=numpy.int32)
+
+#dset.attrs['Units'] = [100, 200]
+f.close()
