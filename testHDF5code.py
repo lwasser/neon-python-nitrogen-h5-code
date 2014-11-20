21ef77211fa6290d9e36ad0411e3fafb8e709943
lwasser <lwasser@neoninc.org>
2014-11-19 20:48:02 -0700

    a few more changes

--- testHDF5code.py
+++ testHDF5code.py
@@ -8,9 +8,9 @@ Created on Wed Nov 19 18:40:32 2014
 import h5py
 import numpy
 f = h5py.File('new.h5', 'a')
-#dset= f['new']
-
+dset= f['new']
+#grp = f.create_group("bar")
 #f['dset'] = numpy.zeros((6,4), dtype=numpy.int32)
-
+#file.visititems(lambda name, object: print(name))
 #dset.attrs['Units'] = [100, 200]
 f.close()
